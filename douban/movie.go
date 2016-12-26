adf9db879b032cabdb9878a3915af5299f549a75
lwh <lwhile@outlook.com>
2016-12-26 14:21:25 +0800

    add

--- douban/movie.go
+++ douban/movie.go
@@ -13,12 +13,26 @@ const (
 )
 
 type Movie struct {
-	Rating  map[string]interface{} `json:"rating"`
-	Genres  []string               `json:"genres"`
-	Title   string                 `json:"title"`
-	Year    string                 `json:"year"`
-	Id      string                 `json:"id"`
-	Summary string                 `json:"summary"`
+	Rating        map[string]interface{}   `json:"rating"`
+	Genres        []string                 `json:"genres"`
+	Title         string                   `json:"title"`
+	Casts         []map[string]interface{} `json:"casts"`
+	CollectCount  int                      `json:"collect_count"`
+	OriginalTitle string                   `json:"original_title"`
+	Subtype       string                   `json:"subtype"`
+	Directors     []map[string]interface{} `json:"directors"`
+	Year          string                   `json:"year"`
+	Images        map[string]string        `json:"images"`
+	Alt           string                   `json:"alt"`
+	Id            string                   `json:"id"`
+	Summary       string                   `json:"summary"`
+	ReviewsCount  int                      `json:"reviews_count"`
+	WishCount     int                      `json:"wish_count"`
+	DoubanSize    string                   `json"douban_size"`
+	MobileUrl     string                   `json:"mobile_url"`
+	DoCount       int                      `json:"do_count"`
+	ShareUrl      string                   `json:"share_url"`
+	ScheduleUrl   string                   `json:"schedule_url"`
 }
 
 type MovieList struct {
@@ -28,23 +42,25 @@ type MovieList struct {
 func SerachMovie(name string, strict bool) (*MovieList, error) {
 	url := RemoteAddr + SerachMovieAPI + name
 	resp, err := http.Get(url)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
 	movieList := MovieList{}
 	body, err := ioutil.ReadAll(resp.Body)
 	json.Unmarshal(body, &movieList)
 	if !strict {
 		return &movieList, nil
 	}
-    newMovieList := MovieList{[]Movie{}}
-    for _, mv := range movieList.Subjects {
-        if mv.Title == name {
-            newMovieList.Subjects = append(newMovieList.Subjects, mv)
-        }
-    }
-    return &newMovieList, nil
+	newMovieList := MovieList{[]Movie{}}
+	for _, mv := range movieList.Subjects {
+		if mv.Title == name {
+			newMovieList.Subjects = append(newMovieList.Subjects, mv)
+		}
+	}
+	return &newMovieList, nil
 }
 
 func MovieInfo(objId string) (*Movie, error) {
--- examples/movie.go
+++ examples/movie.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-    "douban"
-    "fmt"
-    "log"
+	"douban"
+	"fmt"
+	"log"
 )
 
 func main() {
-    movieList, err := douban.SerachMovie("硅谷", true)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(movieList)
-    fmt.Println()
-    movieInfo, err := douban.MovieInfo(movieList.Subjects[0].Id)
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Println(movieInfo)
+	movieList, err := douban.SerachMovie("公司的力量", true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// fmt.Printf("%v\n", movieList)
+	// fmt.Println()
+	movieInfo, err := douban.MovieInfo(movieList.Subjects[0].Id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(movieInfo.Genres)
+	fmt.Println()
+	
 }
